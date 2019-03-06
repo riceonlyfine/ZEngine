600aa40bb38b9ba5d818a967ae3cfdb6d37b073b
riceonlyfine <eldn@live.hk>
2019-03-06 14:18:53 +0800

    Add some basic WebGl initialize things

--- app.ts
+++ app.ts
@@ -1,38 +1,13 @@
 
 
 
-namespace ZE{
-
-    export class Engine {
-
-        private _count : number = 0;
-
-        public constructor(){
-            this._count = 0;
-        }
-
-        public start(): void{
 
 
-            this.loop();
-        }
-
-        private loop(): void{
-            this._count++;
-
-            document.title = this._count.toString();
-            requestAnimationFrame(this.loop.bind(this));
-        }
-    }
-}
-
-
+// the main entry point to hte application.
 window.onload = function(){
 
-    let e = new ZE.Engine();
-    e.start();
-
-    document.body.innerHTML += ',new add text';
+    let engine = new ZE.Engine();
+    engine.start();
 }
 
 
